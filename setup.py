3e0d7a73f039d67173f36db1ee0003f5b1bcbc39
Prins.Wu <prinswu>
2018-09-18 14:47:08 +0800

    modify setup.py and upload by twine to PyPi

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
 
-setup(name='consulservicefinder',
-      version='0.1',
-      description='Find service hav register on consul',
-      url='https://github.com/PrinsWu/consulservicefinder',
-      author='Prins Wu',
-      author_email='prinswu@gmail.com',
-      license='MIT',
-      packages=['consulservicefinder'],
-      zip_safe=False)
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="consulservicefinder",
+    version="0.0.1",
+    author="Prins Wu",
+    author_email="prinswu@gmail.com",
+    description="Find service from Consul",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PrinsWu/consulservicefinder",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
